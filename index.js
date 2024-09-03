e964da45c7938590b634b6b9bde38c9f8c5f5ca4
goushaa <ahmed.elkady@tactful.ai>
2024-09-03 19:13:25 +0300

    seperating my function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const http = require('http');
 const path = require('path');
 const url = require('url')
+const replaceTemplate = require('./modules/replaceTemplate')
 
 /////////////////////////////////////
 // FILES
@@ -31,18 +32,6 @@ const url = require('url')
 
 /////////////////////////////////////
 // SERVER
-const replaceTemplate = (temp,product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName)
-    output = output.replace(/{%IMAGE%}/g,product.image)
-    output = output.replace(/{%PRICE%}/g,product.price)
-    output = output.replace(/{%QUANTITY%}/g,product.quantity)
-    output = output.replace(/{%ID%}/g,product.id)
-    output = output.replace(/{%FROM%}/g,product.from)
-    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients)
-    output = output.replace(/{%DESCRIPTION%}/g,product.description)
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,'not-organic')
-    return output
-}
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
